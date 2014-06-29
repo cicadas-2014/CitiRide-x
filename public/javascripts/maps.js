b801d5f2685e06ffcbffecab023e9742c9cfdbd3
Dinesh Rai & gooby pls <pair+dineshrai.raghav1987@devbootcamp.com>
2014-06-29 19:29:49 -0400

    Center the map properly

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,8 +1,8 @@
 function initialize(coords) {
-      console.log(coords[0][1])
+  console.log(coords);
   var myLatlng = new google.maps.LatLng(coords[0][0],coords[0][1]);
   var mapOptions = {
-    zoom: 10,
+    zoom: 13,
     center: myLatlng
   }
 
@@ -10,77 +10,81 @@ function initialize(coords) {
   displayRoute1walk(coords[0],coords[1]);
   displayRoutebike(coords[1],coords[2]);
   displayRoute2walk(coords[2],coords[3]);
+
 }
 
-function displayRoute1walk(start,start_station) {
-  var directionsService = new google.maps.DirectionsService();
-  var myLatlng = new google.maps.LatLng(start[0],start[1]);
+  function displayRoute1walk(start,start_station) {
+    var directionsService = new google.maps.DirectionsService();
+    var myLatlng = new google.maps.LatLng(start[0],start[1]);
 
-  var myLatlng2 = new google.maps.LatLng(start_station[0],start_station[1]);
+    var myLatlng2 = new google.maps.LatLng(start_station[0],start_station[1]);
 
-  var directionsDisplay = new google.maps.DirectionsRenderer();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
 
-  directionsDisplay.setMap(map);
+    directionsDisplay.setMap(map);
 
-  var request = {
-    origin : myLatlng,
-    destination : myLatlng2,
-    travelMode : google.maps.TravelMode.WALKING
-  };
-  directionsService.route(request, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    }
-  });
-}
+    var request = {
+      origin : myLatlng,
+      destination : myLatlng2,
+      travelMode : google.maps.TravelMode.WALKING
+    };
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setOptions({ preserveViewport: true });
+        directionsDisplay.setDirections(response);
+      }
+    });
+  }
 
-function displayRoutebike(start_station, next_station) {
-  var directionsService = new google.maps.DirectionsService();
-  var myLatlng = new google.maps.LatLng(start_station[0],start_station[1]);
+  function displayRoutebike(start_station, next_station) {
+    var directionsService = new google.maps.DirectionsService();
+    var myLatlng = new google.maps.LatLng(start_station[0],start_station[1]);
 
-  var myLatlng2 = new google.maps.LatLng(next_station[0],next_station[1]);
+    var myLatlng2 = new google.maps.LatLng(next_station[0],next_station[1]);
 
-  var directionsDisplay = new google.maps.DirectionsRenderer();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
 
-  directionsDisplay.setMap(map);
+    directionsDisplay.setMap(map);
 
-  var request = {
-    origin : myLatlng,
-    destination : myLatlng2,
-    travelMode : google.maps.TravelMode.BICYCLING
-  };
-  directionsService.route(request, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    }
-  });
-}
+    var request = {
+      origin : myLatlng,
+      destination : myLatlng2,
+      travelMode : google.maps.TravelMode.BICYCLING
+    };
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setOptions({ preserveViewport: true });
+        directionsDisplay.setDirections(response);
+      }
+    });
+  }
 
 
 
-function displayRoute2walk(next_station, destination) {
-  var directionsService = new google.maps.DirectionsService();
-  var myLatlng = new google.maps.LatLng(next_station[0],next_station[1]);
+  function displayRoute2walk(next_station, destination) {
+    var directionsService = new google.maps.DirectionsService();
+    var myLatlng = new google.maps.LatLng(next_station[0],next_station[1]);
 
-  var myLatlng2 = new google.maps.LatLng(destination[0],destination[1]);
+    var myLatlng2 = new google.maps.LatLng(destination[0],destination[1]);
 
-  var directionsDisplay = new google.maps.DirectionsRenderer();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
 
-  directionsDisplay.setMap(map);
+    directionsDisplay.setMap(map);
 
-  var request = {
-    origin : myLatlng,
-    destination : myLatlng2,
-    travelMode : google.maps.TravelMode.WALKING
-  };
+    var request = {
+      origin : myLatlng,
+      destination : myLatlng2,
+      travelMode : google.maps.TravelMode.WALKING
+    };
 
-  directionsService.route(request, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    }
-  });
-}
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setOptions({ preserveViewport: true });
+        directionsDisplay.setDirections(response);
+      }
+    });
+  }
 
-function renderMap(coords) {
-  google.maps.event.addDomListener(window, 'load', initialize(coords));
-}
+  function renderMap(coords) {
+    google.maps.event.addDomListener(window, 'load', initialize(coords));
+  }
