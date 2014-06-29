d313185fc51f569216772184eefb77c7b7a593b6
Dinesh Rai & gooby pls <pair+dineshrai.raghav1987@devbootcamp.com>
2014-06-29 15:50:52 -0400

    Remove puts statement

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,7 +7,7 @@ class SearchesController < ApplicationController
     start_point_station = find_closest(start_point: params[:s], looking_for: "availableBikes")
     destination_station = find_closest(start_point: params[:d], looking_for: "availableDocks")
 
-    p final_points = {start_coords: start_point_coords , start_station_coords: [start_point_station["latitude"], start_point_station["longitude"]], destination_station_coords: [destination_station["latitude"], destination_station["longitude"]], destination_coords: destination_coords}
+    final_points = {start_coords: start_point_coords , start_station_coords: [start_point_station["latitude"], start_point_station["longitude"]], destination_station_coords: [destination_station["latitude"], destination_station["longitude"]], destination_coords: destination_coords}
   end
 
   def login
