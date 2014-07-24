b2eae5d24e99cf670394a89a8c99c80d43b794a4
Raghav Malik <raghav1987@gmail.com>
2014-07-23 23:52:22 -0400

    Add click feature to icons

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,6 +8,12 @@ class SearchesController < ApplicationController
     destination_station = find_closest(start_point: params[:d], looking_for: "availableDocks")
     @user_input = [params[:s],params[:d]]
     @final_points = [start_point_coords, [start_point_station["latitude"], start_point_station["longitude"]], [destination_station["latitude"], destination_station["longitude"]], destination_coords]
+    
+    # format is ["start_address", "citibike_first_address", "citibike_destination_address", "destination_address"]
+
+    @address = [params[:s], params[:d]]
+    @start_station_data = start_point_station
+    @destination_station_data = destination_station
   end
 
   def login
